946d8e082604e4e16f02ce878c1d3396b4956a4d
Antonio Vinciguerra <antoniovinciguerra@Antonios-Mac-mini.local>
2025-11-06 12:21:36 +0900

    added partial for edit and delete buttons in admin mode

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   # Rooms & bookings (guest side)
-  resources :rooms, only: [:index, :show] do
-    resources :bookings, only: [:new, :create]
+  resources :rooms, only: %i[index show] do
+    resources :bookings, only: %i[new create]
   end
 
   # Bookings with confirmation page
@@ -15,26 +15,25 @@ Rails.application.routes.draw do
   end
 
   # Payments
-  resources :payments, only: [:new, :create]
+  resources :payments, only: %i[new create]
 
   # 'Guests' booking
   resources :guests, only: [:show] do
-    resources :bookings, only: [:index, :show]
+    resources :bookings, only: %i[index show]
   end
-
 
   # Admin only
   namespace :admin do
     resources :rooms
-    resources :bookings, only: [:index, :show]
-    resources :contact_messages, only: [:index, :show, :destroy]
+    resources :bookings, only: %i[index show]
+    resources :contact_messages, only: %i[index show destroy]
   end
 
   # Contact messages (guest side)
-  resources :contact_messages, only: [:new, :create]
+  resources :contact_messages, only: %i[new create]
 
-  get "about_us", to: "pages#about_us"
-  get "faqs", to: "pages#faqs"
+  get 'about_us', to: 'pages#about_us'
+  get 'faqs', to: 'pages#faqs'
 
-  root "pages#home"
+  root 'pages#home'
 end
