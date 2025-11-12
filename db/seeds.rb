6793b39227516d78c2db61fdc568c3ded9e502a8
MaiPhuong NguyenThi <nguyenthimaiphuong2416@gmail.com>
2025-11-12 23:36:48 +0900

    Changed capacity of Room A from 6 to 5 in the seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,7 +50,7 @@ puts "Created #{User.count} users"
     bunk: 1,
     double: 1,
     sofa_bed: 1,
-    capacity: 6,
+    capacity: 5,
     amenities: 'WiFi, Air conditioning, TV, Bathroom'
   )
 end
