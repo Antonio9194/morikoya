96640b2ca1cac8b90caade979bbe6b5afb971e19
Antonio Vinciguerra <antoniovinciguerra@Antonios-Mac-mini.local>
2025-11-13 20:57:43 +0900

    created new index for checkins in controller and added grid to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home about_us faqs]
 
+  require 'date'
+
   def home
+    @bookings = Booking.all
+    @next_checkins = @bookings.where(start_date: (Date.today..Date.today + 2))
   end
 
   def about_us
