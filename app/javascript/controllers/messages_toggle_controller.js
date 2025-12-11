48a14efd589ec57039379a57eacb308d49f1d023
MaiPhuong NguyenThi <nguyenthimaiphuong2416@gmail.com>
2025-12-11 23:29:19 +0900

    Add icons to toggle messages list display on small screen size, using stimulus

---
+++ app/javascript/controllers/messages_toggle_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="messages-toggle"
+export default class extends Controller {
+  static targets = ["messagesList", "placeholderText", "collapseIcon", "expandIcon"]
+
+  connect() {
+    console.log("I will toggle messages list display.")
+  }
+
+  expand() {
+    this.placeholderTextTarget.classList.toggle("d-none");
+    this.messagesListTarget.classList.toggle("d-none");
+    this.collapseIconTarget.classList.toggle("d-none");
+    this.expandIconTarget.classList.toggle("d-none");
+    this.expandIconTarget.style.background = "#E28A96";
+    this.expandIconTarget.style.borderColor = "#ffffffff";
+  }
+
+  collapse() {
+    this.placeholderTextTarget.classList.toggle("d-none");
+    this.messagesListTarget.classList.toggle("d-none");
+    this.collapseIconTarget.classList.toggle("d-none");
+    this.expandIconTarget.classList.toggle("d-none");
+  }
+}
