aee7899806ea0ab341689267d0c5fabe07ad35bf
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-12-31 19:27:49 +0500

    added private method locale to switch language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -8,5 +9,12 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name phone_number email])
+  end
+
+  private
+
+  # Reads the user's language for the session, if not detected goes to english
+  def set_locale
+    I18n.locale = session[:locale] || I18n.default_locale
   end
 end
