a3702969dedb8f9210a5a0db0849a335fbe0c036
Antonio Vinciguerra <antoniovinciguerra@Antonios-Mac-mini.local>
2026-01-07 18:15:01 +0900

    added yml for contactus and fixed authentication to skip when changing language

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,4 +1,5 @@
 class LocalesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :update
   def update
     session[:locale] = params[:locale]
     redirect_back fallback_location: root_path
